face9d89ba067dac00179ae12f8a271d9a6aa24b
adnanioricce <adnangonzagadevio@gmail.com>
2024-07-28 23:24:59 -0300

    Adding a Success/Failure type

--- Program.fs
+++ Program.fs
@@ -1,17 +1,21 @@
 ﻿open System
+type ParseResult<'a> =
+| Success of 'a
+| Failure of string
+
 let pchar (charToMath,str) =
     if String.IsNullOrWhiteSpace(str) |> not then
         let first = str.[0]
         if first = charToMath then
             let remaining = str.[1..]
-            let msg = sprintf "Found %c" charToMath
-            (msg, remaining)
+            //let msg = sprintf "Found %c" charToMath
+            Success (charToMath, remaining)
         else
             let msg = sprintf "Expecting '%c'. Got '%c'" charToMath first
-            (msg,str)        
+            Failure msg        
     else
         let msg = "No more input"
-        (msg,"")
+        Failure msg
             
 let parseA str = pchar ('A',str)
 [<EntryPoint>]
