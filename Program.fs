04efd09f07cd63e8a7271b61d389c578c5d63672
adnanioricce <adnangonzagadevio@gmail.com>
2024-07-28 23:29:42 -0300

    Currying things up
    I dont see why, but I chose to follow the tutorial.

--- Program.fs
+++ Program.fs
@@ -3,21 +3,23 @@ type ParseResult<'a> =
 | Success of 'a
 | Failure of string
 
-let pchar (charToMath,str) =
-    if String.IsNullOrWhiteSpace(str) |> not then
-        let first = str.[0]
-        if first = charToMath then
-            let remaining = str.[1..]
-            //let msg = sprintf "Found %c" charToMath
-            Success (charToMath, remaining)
+let pchar charToMath =
+    let innerFn str = 
+        if String.IsNullOrWhiteSpace(str) |> not then
+            let first = str.[0]
+            if first = charToMath then
+                let remaining = str.[1..]
+                //let msg = sprintf "Found %c" charToMath
+                Success (charToMath, remaining)
+            else
+                let msg = sprintf "Expecting '%c'. Got '%c'" charToMath first
+                Failure msg        
         else
-            let msg = sprintf "Expecting '%c'. Got '%c'" charToMath first
-            Failure msg        
-    else
-        let msg = "No more input"
-        Failure msg
+            let msg = "No more input"
+            Failure msg
+    innerFn
             
-let parseA str = pchar ('A',str)
+let parseA = pchar 'A'
 [<EntryPoint>]
 let main argv =
     printfn "%A " (parseA "ABC")
