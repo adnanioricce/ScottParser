ed1959964495068fb2a22219ab1b172ae6bca0cb
adnanioricce <adnangonzagadevio@gmail.com>
2024-07-28 23:15:37 -0300

    first part - parse 'A' only

---
+++ Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+let parseA str =
+    if String.IsNullOrWhiteSpace(str) then
+        (false,"")
+    else if str.[0] = 'A' then
+        let remaining = str.[1..]
+        (true,remaining)
+    else
+        (false,str)
+[<EntryPoint>]
+let main argv =
+    printfn "%A " (parseA "ABC")
+    printfn "%A " (parseA "ZBC")
+    0
