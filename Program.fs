f34d074ce2f0a998f72e5625ef908f6f3ff6cde9
adnanioricce <adnangonzagadevio@gmail.com>
2024-07-28 23:21:30 -0300

    Refactoring and creating pchar

--- Program.fs
+++ Program.fs
@@ -1,12 +1,19 @@
 ﻿open System
-let parseA str =
-    if String.IsNullOrWhiteSpace(str) then
-        (false,"")
-    else if str.[0] = 'A' then
-        let remaining = str.[1..]
-        (true,remaining)
+let pchar (charToMath,str) =
+    if String.IsNullOrWhiteSpace(str) |> not then
+        let first = str.[0]
+        if first = charToMath then
+            let remaining = str.[1..]
+            let msg = sprintf "Found %c" charToMath
+            (msg, remaining)
+        else
+            let msg = sprintf "Expecting '%c'. Got '%c'" charToMath first
+            (msg,str)        
     else
-        (false,str)
+        let msg = "No more input"
+        (msg,"")
+            
+let parseA str = pchar ('A',str)
 [<EntryPoint>]
 let main argv =
     printfn "%A " (parseA "ABC")
